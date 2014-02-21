814842e0cd551f04f4a52ed6df653af478652175
Erik van Seters <github@dexyon.com>
2014-02-21 22:06:49 +0100

    Set-up commit for our list abstraction project

---
+++ MvvmCross-ObjectList/Attributes/BindableListItem.cs
@@ -0,0 +1,26 @@
+﻿namespace Dexyon.MvvmCrossObjectList.Attributes {
+
+	[System.AttributeUsage(System.AttributeTargets.Property, AllowMultiple = true)]
+	public class BindableListItem : System.Attribute
+	{
+
+		/// <summary>
+		/// Gets or sets the description.
+		/// </summary>
+		/// <value>The description.</value>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Gets or sets the value.
+		/// </summary>
+		/// <value>The value.</value>
+		public string ValueConverter { get; set; }
+
+		/// <summary>
+		/// Gets or sets the order.
+		/// </summary>
+		/// <value>The order.</value>
+		public int Order { get; set; }
+	}
+}
+
