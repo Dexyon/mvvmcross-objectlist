1a444cde26336afc64f1ea53dc69263ad5e96656
Erik van Seters <github@dexyon.com>
2014-02-21 22:55:18 +0100

    App set-up for using MvvmCross

---
+++ MvvmCross-ObjectList.Core/App.cs
@@ -0,0 +1,13 @@
+﻿using Cirrious.MvvmCross.ViewModels;
+using Dexyon.MvvmCrossObjectList.Core.ViewModels;
+
+namespace Dexyon.MvvmCrossObjectList.Core {
+	public class App : MvxApplication
+	{
+		public App()
+		{
+			RegisterAppStart<ExampleOverviewViewModel>();
+		}
+	}
+}
+
