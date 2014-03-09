7638007f1fdc9ac4124b3a6390f844022101a668
Erik van Seters <github@dexyon.com>
2014-03-09 13:44:47 +0100

    Added a sample Converter to our project

---
+++ MvvmCross-ObjectList.Touch/Converters/AgeConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Cirrious.CrossCore.Converters;
+
+namespace Dexyon.MvvmCrossObjectList.Touch {
+	public class AgeConverter : MvxValueConverter<DateTime, string> {
+		protected override string Convert (DateTime value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+			return "Test";
+		}
+
+		protected override DateTime ConvertBack (string value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+			return new DateTime ( 1970, 1, 1 );
+		}
+	}
+}
+
