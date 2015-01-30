8905540e9bf2d2b67b7417602852a69a3df92ec4
Erik van Seters <github@dexyon.com>
2015-01-30 22:18:53 +0100

    Extra assembly info Updated version number

--- MvvmCross-ObjectList.Proxy/MvvmCross-ObjectList.Droid.UILib/Properties/AssemblyInfo.cs
+++ MvvmCross-ObjectList.Proxy/MvvmCross-ObjectList.Droid.UILib/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ( "MvvmCross-ObjectList.Touch.UILib" )]
+[assembly: AssemblyTitle ( "MvvmCross-ObjectList.Droid.UILib" )]
 [assembly: AssemblyDescription ( "" )]
 [assembly: AssemblyConfiguration ( "" )]
 [assembly: AssemblyCompany ( "Dexyon" )]
