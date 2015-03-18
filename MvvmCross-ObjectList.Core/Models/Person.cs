d0ec4927178588b873f41c1a0bfbbbf6291ad7f2
Erik van Seters <github@dexyon.com>
2015-03-18 07:31:23 +0100

    Added localisation to the Model

--- MvvmCross-ObjectList.Core/Models/Person.cs
+++ MvvmCross-ObjectList.Core/Models/Person.cs
@@ -7,17 +7,21 @@ namespace Dexyon.MvvmCrossObjectList.Core.Models
 	{
 
 		[ProxyModelAttribute(Description = "Full Name:", Order = 1 )]
+		[MvxLang(Text="Fullname", TextSource="TextSource")]
 		public string FullName { get; set; }
 
 		[ProxyModelAttribute(Description = "Birth Place:", Order = 3 )]
+		[MvxLang(Text="BirthPlace", TextSource="TextSource")]
 		public string BirthPlace { get; set; }
 
 		[ProxyModelAttribute(Description = "Date of Birth:", Order = 4 )]
+		[MvxLang(Text="BirthDate", TextSource="TextSource")]
 		public DateTime BirthDate {get;set;}
 
 		//public bool HasChildren { get; set;}
 
 		[ProxyModelAttribute(Description = "Age:", Order = 2 )]
+		[MvxLang(Text="Age", TextSource="TextSource")]
 		public int Age
 		{
 			get{ 
@@ -26,6 +30,7 @@ namespace Dexyon.MvvmCrossObjectList.Core.Models
 		}
 
 		[ProxyModelAttribute(Description = "Salary: ")]
+		[MvxLang(Text="Salary", TextSource="TextSource")]
 		public double Salary { get; set; }
 
 	}
