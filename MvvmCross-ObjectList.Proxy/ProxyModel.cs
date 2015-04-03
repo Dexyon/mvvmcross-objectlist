67b71ce7b21cc40b7a7dcc5c666d108914b59796
Erik van Seters <github@dexyon.com>
2015-04-03 21:45:26 +0200

    Updated the ProxyModel to also allow a single ViewModel. This allows easier integration of the component in existing ViewModels / codebases. Added a ProxyProperty to set which properties should be used.

--- MvvmCross-ObjectList.Proxy/ProxyModel.cs
+++ MvvmCross-ObjectList.Proxy/ProxyModel.cs
@@ -7,34 +7,75 @@ using Cirrious.CrossCore;
 using Cirrious.MvvmCross.Localization;
 using Cirrious.MvvmCross.ViewModels;
 
-namespace Dexyon.MvvmCrossObjectList.Proxy
-{
-	public class ProxyModel<T, TViewModel> : ObservableCollection<ProxyProperty> 
-		where TViewModel : MvxViewModel 
-	{
+namespace Dexyon.MvvmCrossObjectList.Proxy {
+	public class ProxyModel<TViewModel> : BaseProxyModel<TViewModel>
+		where TViewModel : MvxViewModel {
 
-		private const string TextProvider = "TextProvider";
-		private T _baseModel;
-		private TViewModel _tViewModel;
-		Action _notifyChangedDelegate;
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Dexyon.MvvmCrossObjectList.Proxy.ProxyModel`1"/> class.
+		/// </summary>
+		/// <param name="viewModel">View model.</param>
+		/// <param name="raiseThisPropertyChanged">Raise this property changed.</param>
+		/// <param name="notifyAllProperties">If set to <c>true</c> notify all properties.</param>
+		public ProxyModel ( TViewModel viewModel, Action raiseThisProperyChanged = null )
+			: base ( viewModel, PropertyMode.OnlyDecoratedProperties, raiseThisProperyChanged, false ) {
+
+			//Create proxy property list
+			CreateProxyPropertyList (
+				viewModel, typeof(TViewModel).GetProperties ().Where( 
+					prop => prop.GetCustomAttributes(typeof(ProxyModelAttribute), false).Any() ) );
+		}
+	}
+	
+
+	public class ProxyModel<T, TViewModel> : BaseProxyModel<TViewModel> 
+		where TViewModel : MvxViewModel {
 
 		/// <summary>
 		/// Gets the base model. This model is used for the get and the set actions performed on the properties.
 		/// </summary>
 		/// <value>The base model.</value>
-		public T BaseModel { 
-			get { return _baseModel; } 
+		public T BaseModel { get; private set; }
+
+		public ProxyModel ( T baseModel, TViewModel viewModel, PropertyMode propertyMode, Action raiseThisPropertyChanged = null, bool notifyAllProperties = true )
+			: base ( viewModel, propertyMode, raiseThisPropertyChanged, notifyAllProperties ) {
+			BaseModel = baseModel;
+
+			//Create proxy property list
+			switch ( propertyMode ) {
+			case PropertyMode.OnlyDecoratedProperties:
+				CreateProxyPropertyList (
+					viewModel, typeof(T).GetProperties ().Where( 
+						prop => prop.GetCustomAttributes(typeof(ProxyModelAttribute), false).Any() ) );
+				break;
+			default:
+				CreateProxyPropertyList ( baseModel, typeof(T).GetProperties () );
+				break;
+			}
 		}
 
-		public ProxyModel (T baseModel, TViewModel viewModel, Action raiseThisPropertyChanged = null, bool notifyAllProperties = true)
-		{
-			NotifyAllProperties = notifyAllProperties;
-			_baseModel = baseModel;
-			_tViewModel = viewModel;
-			_notifyChangedDelegate = raiseThisPropertyChanged;
-			//Create proxy property list
-			CreateProxyPropertyList (_baseModel);
+		public ProxyModel ( T baseModel, TViewModel viewModel, Action raiseThisPropertyChanged = null, bool notifyAllProperties = true )
+			: this ( baseModel, viewModel, PropertyMode.AllPublicProperties, raiseThisPropertyChanged, notifyAllProperties ) {
+		}
+	}
 
+	public abstract class BaseProxyModel<TViewModel> : ObservableCollection<ProxyProperty>  {
+
+		private const string TextProvider = "TextProvider";
+		private readonly Action _notifyChangedDelegate;
+
+		protected TViewModel _viewModel;
+		protected PropertyMode _propertyMode;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Dexyon.MvvmCrossObjectList.Proxy.BaseProxyModel`1"/> class.
+		/// </summary>
+		/// <param name="propertyMode">The mode to determin which properties should be read.</param>
+		/// <param name="viewModel">The ViewModel on which the proxy property is placed.</param>
+		protected BaseProxyModel ( TViewModel viewModel, PropertyMode propertyMode, Action raiseThisPropertyChanged = null, bool notifyAllProperties = true  ) {
+			this._viewModel = viewModel;
+			this._propertyMode = propertyMode;
+			this._notifyChangedDelegate = raiseThisPropertyChanged;
 		}
 
 		/// <summary>
@@ -42,14 +83,56 @@ namespace Dexyon.MvvmCrossObjectList.Proxy
 		/// all properties when only one single property is changed.
 		/// </summary>
 		/// <value><c>true</c> if notify all properties; otherwise, <c>false</c>.</value>
-		public bool NotifyAllProperties { get; set;}
+		public bool NotifyAllProperties { get; set; }
 
-		private void CreateProxyPropertyList (T instance) {
+		/// <summary>
+		/// Gets the typed attribute from a propert
+		/// </summary>
+		/// <returns>The typed attribute or NULL if nothing could be found.</returns>
+		/// <param name="info">The property from which the attribute should be read.</param>
+		/// <typeparam name="TReturnType">The 1st type parameter.</typeparam>
+		protected static TReturnType GetTypedAttribute<TReturnType> ( PropertyInfo info ) where TReturnType : class {
+			// Get the attributes from our Attribute component
+			var attrs = new List<Attribute> ( info.GetCustomAttributes ( typeof(TReturnType), false ) );
+			return attrs.Any () ? (attrs [0]) as TReturnType : null; 
+		}
 
-			// Get all properties from our object
-			IList<PropertyInfo> props = typeof(T).GetRuntimeProperties().ToList();
+		/// <summary>
+		/// Converts a description using the MvxLang capabilities.
+		/// </summary>
+		/// <returns>The translated description.</returns>
+		/// <param name="item">The property on which the attributes are set</param>
+		/// <param name="objectProperty">The proxy model attribute read from the item</param>
+		/// <param name="mvxLangAttribute">The mvxlang attribute read from the item</param>
+		/// <param name="viewModel">The view model from which the property was read.</param>
+		protected static string ConvertDescription ( PropertyInfo item, ProxyModelAttribute objectProperty, MvxLangAttribute mvxLangAttribute, TViewModel viewModel ) {
+			if ( mvxLangAttribute == null ) {
+				return ConvertDescription ( item, objectProperty );
+			}
 
-			foreach ( var item in props ) {
+			try {
+				var provider = mvxLangAttribute.TextSource ?? TextProvider;
+				var property = viewModel.GetType ().GetProperty ( provider ).GetValue ( viewModel ) as IMvxLanguageBinder;
+
+				if ( property != null ) {
+					return property.GetText ( mvxLangAttribute.Text );
+				}
+			} catch ( Exception GE ) {
+				// Fallback
+				Mvx.Error ( "Error while execuring MvxLang with error: {0}", GE );
+			}
+
+			return ConvertDescription ( item, objectProperty );
+		}
+
+		private static string ConvertDescription ( PropertyInfo item, ProxyModelAttribute objectProperty ) {
+			return objectProperty != null && !string.IsNullOrEmpty ( objectProperty.Description ) 
+				? objectProperty.Description 
+					: item.Name;
+		}
+
+		protected void CreateProxyPropertyList<TObject> ( TObject instance, IEnumerable<PropertyInfo> properties ) {
+			foreach ( var item in properties ) {
 
 				var proxyModelAttribute = GetTypedAttribute<ProxyModelAttribute> ( item );
 				var mvxLangAttribute = GetTypedAttribute<MvxLangAttribute> ( item );
@@ -58,78 +141,40 @@ namespace Dexyon.MvvmCrossObjectList.Proxy
 				ProxyProperty proxyProp = new ProxyProperty(
 					item.PropertyType,											// Value type
 					ConvertDescription(											// Converted description
-						item, proxyModelAttribute, mvxLangAttribute, _tViewModel
+						item, proxyModelAttribute, mvxLangAttribute,_viewModel
 					),				
 					item.Name, 													// The original description name
 					item.CanWrite ?												// Setter (if exists)
-						new Action<object>((v)=> {
-							item.SetValue(instance,v,null);
-							NotifyChanged();
-						})
-						: null, 												
+					new Action<object>((v)=> {
+						item.SetValue(instance,v,null);
+						NotifyChanged();
+					})
+					: null, 												
 					new Func<object>(()=>item.GetValue(instance,null)), 		// Getter
 					ExecuteNotifyAllProperties									// Notifies other properties
 				);
 
 				// Add it to the list
 				Add (proxyProp);
-	
 			}
 		}
 
-		private static TReturnType GetTypedAttribute<TReturnType>( PropertyInfo info ) where TReturnType : class 
-		{
-			// Get the attributes from our Attribute component
-			var attrs = new List<Attribute>( info.GetCustomAttributes ( typeof(TReturnType), false ));
-			return attrs.Any () ? (attrs [0]) as TReturnType : null; 
-		}
-
-		private void NotifyChanged()
-		{
-			if (_notifyChangedDelegate != null)
+		private void NotifyChanged () {
+			if ( _notifyChangedDelegate != null )
 				_notifyChangedDelegate ();
 		}
 
-		private void ExecuteNotifyAllProperties(ProxyProperty sender)
-		{
-			foreach(var prop in this)
-			{
-				if (prop != sender) {
+		private void ExecuteNotifyAllProperties ( ProxyProperty sender ) {
+			if ( !NotifyAllProperties ) {
+				return;
+			}
+
+			foreach ( var prop in this ) {
+				if ( prop != sender ) {
 					prop.MyValueChangedAsWell ();
 				}
 			}
 		}
-
-		private static string ConvertDescription (PropertyInfo item, ProxyModelAttribute objectProperty, MvxLangAttribute mvxLangAttribute, TViewModel viewModel) 
-		{
-			if (mvxLangAttribute == null) 
-			{
-				return ConvertDescription ( item, objectProperty );
-			}
-
-			try {
-				var provider = mvxLangAttribute.TextSource ?? TextProvider;
-				var property = viewModel.GetType ().GetProperty ( provider ).GetValue ( viewModel ) as IMvxLanguageBinder;
-
-				if (property != null) {
-					return property.GetText(mvxLangAttribute.Text);
-				}
-			} 
-			catch (Exception GE) {
-				// Fallback
-				Mvx.Error("Error while execuring MvxLang with error: {0}", GE);
-			}
-
-			return ConvertDescription ( item, objectProperty );
-		}
-
-		private static string ConvertDescription (PropertyInfo item, ProxyModelAttribute objectProperty) 
-		{
-			return objectProperty != null && !string.IsNullOrEmpty(objectProperty.Description) 
-				? objectProperty.Description 
-					: item.Name;
-		}
-
 	}
 }
 
--- MvvmCross-ObjectList.Proxy/ProxyProperty.cs
+++ MvvmCross-ObjectList.Proxy/ProxyProperty.cs
@@ -1,24 +1,17 @@
 ﻿using System;
 using System.ComponentModel;
 
-namespace Dexyon.MvvmCrossObjectList.Proxy
-{
-	public class ProxyProperty : INotifyPropertyChanged
-	{
+namespace Dexyon.MvvmCrossObjectList.Proxy {
+	public class ProxyProperty : INotifyPropertyChanged {
 		private Action<object> _valueSetter;
 		private Func<object> _valueGetter;
 		private Type _valueType;
 		private Action<ProxyProperty> _notifyValueChanged;
 
-		public ProxyProperty ()
-		{
-		}
-
-		internal ProxyProperty (Type valueType, string description, string originalDescription, Action<object> valueSetter, Func<object> valueGetter, Action<ProxyProperty> notifyValueChanged)
-		{
+		internal ProxyProperty ( Type valueType, string description, string propertyName, Action<object> valueSetter, Func<object> valueGetter, Action<ProxyProperty> notifyValueChanged ) {
 			_valueType = valueType;
 			Description = description;
-			OriginalDescription = originalDescription;
+			PropertyName = propertyName;
 			_valueGetter = valueGetter;
 			_valueSetter = valueSetter;
 
@@ -32,33 +25,28 @@ namespace Dexyon.MvvmCrossObjectList.Proxy
 		#endregion
 
 		public object Value {
-			get{
-				if (_valueGetter != null) {
-					return _valueGetter ();
-				} else {
-					return null;
-				}
+			get {
+				return _valueGetter != null ? _valueGetter () : null;
 			}
-			set{
-				if (_valueSetter != null) {
-					_valueSetter (value);
+			set {
+				if ( _valueSetter != null ) {
+					_valueSetter ( value );
 
-					MyValueChangedAsWell();
+					MyValueChangedAsWell ();
 
-					if (_notifyValueChanged != null)
-						_notifyValueChanged (this);
+					if ( _notifyValueChanged != null )
+						_notifyValueChanged ( this );
 				} 
 			}
 		}
 
-		internal void MyValueChangedAsWell()
-		{
-			if (PropertyChanged != null) {
-				PropertyChanged (this, new PropertyChangedEventArgs ("Value"));
+		internal void MyValueChangedAsWell () {
+			if ( PropertyChanged != null ) {
+				PropertyChanged ( this, new PropertyChangedEventArgs ( "Value" ) );
 			}
 		}
 
-		public string OriginalDescription {
+		public string PropertyName {
 			get;
 			private set;
 		}
@@ -68,14 +56,14 @@ namespace Dexyon.MvvmCrossObjectList.Proxy
 			set;
 		}
 
-		public bool IsReadOnly{
-			get{ 
+		public bool IsReadOnly {
+			get { 
 				return _valueSetter == null;
 			}
 		}
 
-		public Type ValueType{
-			get{ 
+		public Type ValueType {
+			get { 
 				return _valueType;
 			}
 		}
