db22585b005d83c81b3c4afcbcbb2ca1944d2c25
Erik van Seters <github@dexyon.com>
2015-04-09 21:33:16 +0200

    Allow setting of the binding from within the android binding (iOS will follow)

--- MvvmCross-ObjectList.Proxy/MvvmCross-ObjectList.Droid.UILib/ObjectListAdapter.cs
+++ MvvmCross-ObjectList.Proxy/MvvmCross-ObjectList.Droid.UILib/ObjectListAdapter.cs
@@ -1,48 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Android.Views;
-using Cirrious.CrossCore;
-using Cirrious.MvvmCross.Binding.Binders;
 using Cirrious.MvvmCross.Binding.BindingContext;
-using Cirrious.MvvmCross.Binding.Bindings;
 using Cirrious.MvvmCross.Binding.Droid.BindingContext;
 using Cirrious.MvvmCross.Binding.Droid.Views;
 using Dexyon.MvvmCrossObjectList.Proxy;
-using Android.Widget;
 
 namespace MvvmCrossObjectList.Droid.UILib
 {
-	public class ObjectListAdapter : MvxAdapter
-	{
+	public class ObjectListAdapter : MvxAdapter {
 
-		private readonly IMvxBindingDescriptionParser _bindingParser;
-
-		public ObjectListAdapter (Android.Content.Context context, IMvxAndroidBindingContext bindingContext)
-			: base(context, bindingContext)
-		{
-			this._bindingParser = Mvx.Resolve<IMvxBindingDescriptionParser>();
-		}
-
+		private const string DEFAULT_TEXT_BINDING = "Text Value";
 		private bool _setup = true;
 		private List<TemplateSelector> _templateSelectors = null;
+		private ListTemplate _defaultListTemplate;
 
-		public void Setup(List<TemplateSelector> templateSelectors)
-		{
-			_templateSelectors = templateSelectors;
-			_setup &= _templateSelectors == null;
+		public ObjectListAdapter ( Android.Content.Context context, IMvxAndroidBindingContext bindingContext )
+			: base ( context, bindingContext ) {
 		}
 
-		public override int GetItemViewType (int position)
-		{
-			if (_setup)
+		public void Setup ( List<TemplateSelector> templateSelectors, ListTemplate defaultListTemplate ) {
+			_templateSelectors = templateSelectors;
+			_setup &= _templateSelectors == null;
+			_defaultListTemplate = defaultListTemplate;
+		}
+
+		public override int GetItemViewType ( int position ) {
+			if ( _setup )
 				return 0;
 
-			var item = GetRawItem (position) as ProxyProperty;
+			var item = GetRawItem ( position ) as ProxyProperty;
 
-			for (int i = 0; i < _templateSelectors.Count; i++) {
-				if (_templateSelectors [i].Condition (item))
+			for ( int i = 0; i < _templateSelectors.Count; i++ ) {
+				if ( _templateSelectors [i].Condition ( item ) )
 					return i;
-			} 
+			}
+
+			// return the template ID of the default. 
 			//nothing applies
 			return 0;
 		}
@@ -50,73 +45,149 @@ namespace MvvmCrossObjectList.Droid.UILib
 		public override int ViewTypeCount {
 			get { 
 				return _setup ? 1 : _templateSelectors.Count;
-
 			}
 		}
 
-		protected override View GetBindableView (View convertView, object source, int templateId)
-		{
+		protected override View GetBindableView ( View convertView, object source, int templateId ) {
 			if (_setup)
 				return base.GetBindableView (convertView, source, templateId);
 
-			foreach (var sel in _templateSelectors) {
-				if (sel.Condition (source as ProxyProperty)) {
-					templateId = sel.TemplateId;
-					break;
+			ProxyProperty castedProperty = source as ProxyProperty;
+
+			TemplateSelector templateSelector = 
+				_templateSelectors.FirstOrDefault ( sel => sel.Condition ( castedProperty ) );
+
+			// Get the list template to use (default or override)
+			var listTemplate = GetListTemplate (_defaultListTemplate, templateSelector);
+
+			// Override the template ID. 
+			templateId = listTemplate.TemplateId;	
+
+			// Source: http://stackoverflow.com/questions/28985504/mvxadapter-programmatic-binding
+			if ( convertView == null ) {
+				convertView = base.GetBindableView ( convertView, source, templateId );
+
+				string bindingText = GetBindingText ( templateSelector, castedProperty );
+
+				if ( listTemplate != null && !string.IsNullOrEmpty(bindingText) ) {
+
+					var des = convertView.FindViewById (listTemplate.DescriptionField);
+					var tex = convertView.FindViewById (listTemplate.TextField);
+
+					var owner = convertView as IMvxBindingContextOwner;
+					owner.CreateBinding ( des ).For ( "Text" ).To ( "Description" ).Apply ();
+					owner.AddBindings ( tex, bindingText );
 				}
+			} else {
+				BindBindableView ( source, convertView as IMvxListItemView );
 			}
 
-			return base.GetBindableView (convertView, source, templateId);
+			return convertView;
 		}
 
-		/*protected override IMvxListItemView CreateBindableView (object dataContext, int templateId) {
-			var bindable =  base.CreateBindableView (dataContext, templateId);
-
-			TemplateSelector selector = null;
-
-			foreach (var sel in _templateSelectors) {
-				if (sel.Condition (source as ProxyProperty)) {
-					selector = sel;
-					break;
-				}
+		/// <summary>
+		/// Gets the binding text.
+		/// First we focus on the Droid binding (override of the default proxyProperty).
+		/// Then we check if we have defined a binding in the proxyProperty.
+		/// When we haven't defined any binding's at all we use the default binding.
+		/// </summary>
+		/// <returns>The binding text.</returns>
+		/// <param name="templateSelector">Template selector.</param>
+		/// <param name="proxyProperty">Proxy property.</param>
+		private static string GetBindingText ( TemplateSelector templateSelector, ProxyProperty proxyProperty ) {
+			if ( templateSelector != null && !string.IsNullOrEmpty ( templateSelector.Binding ) ) {
+				return templateSelector.Binding;
+			} 
+			if ( proxyProperty != null && !string.IsNullOrEmpty ( proxyProperty.BindingText ) ) {
+				return proxyProperty.BindingText;
 			}
+			return DEFAULT_TEXT_BINDING;
+		}
 
-			// Check if we need to set a custom binding
-			var mvxBindingContextOwner = viewToUse as IMvxBindingContextOwner;
-
-			if ( mvxBindingContextOwner != null && _bindingParser != null 
-				&& selector != null && !string.IsNullOrEmpty(selector.Binding) ) {
-				var bindingDescriptors = new List<MvxBindingDescription>( _bindingParser.Parse (selector.Binding));
-
-				mvxBindingContextOwner.AddBindings ( viewToUse, bindingDescriptors );
-			}
-
-			return bindable;
-
-		}*/
+		/// <summary>
+		/// Pick the correct List template (the default given at set-up or an override defined).
+		/// </summary>
+		/// <returns>The list template.</returns>
+		/// <param name="defaultTemplate">Default template.</param>
+		/// <param name="templateSelector">Template selector.</param>
+		private static ListTemplate GetListTemplate ( ListTemplate defaultTemplate, TemplateSelector templateSelector ) {
+			return templateSelector != null && templateSelector.ListTemplate != null 
+					? templateSelector.ListTemplate
+					: defaultTemplate; 
+		}
 	}
 
-	public class TemplateSelector
-	{
-		/*public TemplateSelector(Predicate<ProxyProperty> condition) 
-			: this ( condition, Resource.Layout.ListItem_ReadOnly)
-		{ }*/
+	public class TemplateSelector {
 
-		public TemplateSelector(Predicate<ProxyProperty> condition, int templateId)
-		{
+		public TemplateSelector ( Predicate<ProxyProperty> condition ) {
 			Condition = condition;
-			TemplateId = templateId;
 		}
 
-		public TemplateSelector(Predicate<ProxyProperty> condition, int templateId, string bindingDescription) 
-			: this (condition, templateId)
-		{
+		public TemplateSelector ( Predicate<ProxyProperty> condition, string bindingDescription ) 
+			: this ( condition ) {
 			Binding = bindingDescription;
 		}
 
-		public Predicate<ProxyProperty> Condition {get;set;}
-		public int TemplateId {get;set;}
-		public string Binding { get; private set;}
+		public TemplateSelector ( Predicate<ProxyProperty> condition, ListTemplate listTemplate ) {
+			this.Condition = condition;
+			this.ListTemplate = listTemplate;
+		}
+
+		public TemplateSelector ( Predicate<ProxyProperty> condition, ListTemplate listTemplate, string bindingDescription )
+			: this ( condition, listTemplate ) {
+			Binding = bindingDescription;
+		}
+
+		/// <summary>
+		/// The condition which should be met to use this selector
+		/// </summary>
+		/// <value>The condition.</value>
+		public Predicate<ProxyProperty> Condition { get; private set; }
+
+		/// <summary>
+		/// Gets the list template.
+		/// </summary>
+		/// <value>The list template.</value>
+		public ListTemplate ListTemplate { get; private set; }
+
+		/// <summary>
+		/// The custom binding
+		/// </summary>
+		/// <value>The binding.</value>
+		public string Binding { get; private set; }
 	}
+
+	public class ListTemplate {
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MvvmCrossObjectList.Droid.UILib.ListTemplate"/> class.
+		/// </summary>
+		/// <param name="templateId">Template identifier.</param>
+		/// <param name="descriptionField">Description field.</param>
+		/// <param name="textField">Text field.</param>
+		public ListTemplate ( int templateId, int descriptionField, int textField ) {
+			this.TemplateId = templateId;
+			this.DescriptionField = descriptionField;
+			this.TextField = textField;
+		}
+
+		/// <summary>
+		/// The android template ID
+		/// </summary>
+		/// <value>The template identifier.</value>
+		public int TemplateId { get; private set; }
+
+		/// <summary>
+		/// Gets the description view.
+		/// </summary>
+		/// <value>The description view.</value>
+		public int DescriptionField { get; private set; }
+
+		/// <summary>
+		/// Gets the text view.
+		/// </summary>
+		/// <value>The text view.</value>
+		public int TextField { get; private set; }
+	} 
 }
 
--- Sample/MvvmCross-ObjectList.Droid/Views/ExampleOverviewView.cs
+++ Sample/MvvmCross-ObjectList.Droid/Views/ExampleOverviewView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.App;
 using Android.OS;
 using Cirrious.MvvmCross.Binding.Droid.BindingContext;
@@ -12,35 +13,32 @@ namespace Dexyon.MvvmCrossObjectList.Droid {
 
 		ObjectListAdapter adapter;
 
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
-			adapter = new ObjectListAdapter (this.BaseContext, (IMvxAndroidBindingContext)this.BindingContext);
+		protected override void OnCreate ( Bundle bundle ) {
+			base.OnCreate ( bundle );
+			adapter = new ObjectListAdapter ( this.BaseContext, (IMvxAndroidBindingContext)this.BindingContext );
 
-			//One layout to rule them all
-			adapter.Setup (new System.Collections.Generic.List<TemplateSelector> ()
-				{ 
-					new TemplateSelector(x=> x.OriginalDescription == "Salary", Resource.Layout.listitem_readonly, 
-						"Text Value, Converter=Salary"),
-					new TemplateSelector(c=>c.IsReadOnly, Resource.Layout.listitem_readonly),
-					//new TemplateSelector((c)=>c.ValueType == typeof(bool),Resource.Layout.ListItem_Bool),
-					new TemplateSelector(c=>c.OriginalDescription == "Age", Resource.Layout.listitem_readonly),
-					new TemplateSelector(c=>c.ValueType == typeof(DateTime),Resource.Layout.ListItem_DatePicker),
-					new TemplateSelector(c=>true,Resource.Layout.ListItem_TextEdit),
+			var defaultListTemplate = new ListTemplate (
+				Resource.Layout.ListItem_TextEdit, Resource.Id.MainCustom_Description, Resource.Id.MainCustom_Value );
+			var dateTimeListTemplate = new ListTemplate ( 
+				Resource.Layout.ListItem_DatePicker, Resource.Id.MainCustom_Description, Resource.Id.MainCustom_Value );
+			var editListTemplate = new ListTemplate ( 
+				Resource.Layout.ListItem_TextEdit, Resource.Id.MainCustom_Description, Resource.Id.MainCustom_Value );
 
-				}
-			);
+			//One layout to rule them all. Update with default (template and views, and views)
+			adapter.Setup (
+				new List<TemplateSelector> { 
+					new TemplateSelector ( x => x.OriginalDescription == "Salary", "Text Value, Converter=Salary" ),
+					new TemplateSelector ( c => c.IsReadOnly || c.OriginalDescription == "Age" ),
+					new TemplateSelector ( c => c.ValueType == typeof(DateTime), dateTimeListTemplate ),
+					new TemplateSelector ( c => true, editListTemplate ),
+				}, 
+				defaultListTemplate );
 
 			// Set our view from the "main" layout resource
 			SetContentView ( Resource.Layout.Main );
 
-			var list = FindViewById<MvxListView> (Resource.Id.PersonList);
+			var list = FindViewById<MvxListView> ( Resource.Id.PersonList );
 			list.Adapter = adapter;
-		}
-		 
-		protected override void OnViewModelSet()
-		{
-
 		}
 	}
 }
